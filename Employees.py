8d4e12acd13177764181b4ec20b6a9e408bb3745
IvánG <ivangarciaganado@gmail.com>
2016-02-17 12:38:43 +0100

    v2

--- Employees.py
+++ Employees.py
@@ -9,3 +9,18 @@
 # Licence:     <your licence>
 #-------------------------------------------------------------------------------
 import arcpy
+import random
+import time
+
+#Variables
+arcpy.env.workspace = r"C:\Student\PFM\PFM.gdb\NuclearPowerPlant\Employees"
+Employees = arcpy.env.workspace
+MoveX = random.uniform(-50,50)
+MoveY = random.uniform(-50,50)
+
+#Actualizar coordenadas
+with arcpy.da.UpdateCursor(Employees, ["SHAPE@XY"]) as cursor:
+    for row in cursor:
+        cursor.updateRow([[row[0][0] + MoveX,row[0][1] + MoveY]])
+
+print "Script Completed"

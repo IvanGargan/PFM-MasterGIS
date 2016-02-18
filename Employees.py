833361836e3559cddbf9baee388aed46882ff8c7
IvánG <ivangarciaganado@gmail.com>
2016-02-18 11:59:08 +0100

    v5

--- Employees.py
+++ Employees.py
@@ -12,25 +12,28 @@
 import arcpy
 import random
 import time
-
 #Sobreescribir
 arcpy.env.overwriteOutput = True
 
-#Variables
+#Parametros
 arcpy.env.workspace = r"C:\Student\PFM\PFM.gdb"#arcpy.GetParameterAsText(0) #Workspace
 Employees = r"C:\Student\PFM\PFM.gdb\NuclearPowerPlant\Employees" #arcpy.GetParameter(1) #FeatureClass
-MoveX = random.uniform(-50,50) #Genera numero aleatorio entre -50 y 50 y...
-MoveY = random.uniform(-50,50) #...lo suma a las coordenadas del empleado.
 
-#Actualizar coordenadas con el cursor
-with arcpy.da.UpdateCursor(Employees, ["SHAPE@XY"]) as cursor:
-    for row in cursor:
-        cursor.updateRow([[row[0][0] + MoveX,row[0][1] + MoveY]])
+while True:
+    #Variables
+    MoveX = random.uniform(-5,5) #Genera numero aleatorio entre -50 y 50 y...
+    MoveY = random.uniform(-5,5) #...lo suma a las coordenadas del empleado.
 
-#Creacion de una layer para evitar locks
-lyr = arcpy.MakeFeatureLayer_management("Employees","Employees_layer")
+    #Actualizar coordenadas con el cursor
+    with arcpy.da.UpdateCursor(Employees, ["SHAPE@XY"]) as cursor:
+        for row in cursor:
+            cursor.updateRow([[row[0][0] + MoveX,row[0][1] + MoveY]])
+
+    #Creacion de una layer para evitar locks
+    lyr = arcpy.MakeFeatureLayer_management("Employees","Employees_layer")
+
+    print "Script Completed"
 
 #Intervalo de tiempo en el que se "mueven" los empleados
-time.sleep(5)
+    time.sleep(20)
 
-print "Script Completed"

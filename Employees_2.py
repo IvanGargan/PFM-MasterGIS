b4f4d3ea02edf116bc3437d9644fdd8b9510e547
IvánG <ivangarciaganado@gmail.com>
2016-03-21 11:59:22 +0100

    Employees_2

--- Employees_2.py
+++ Employees_2.py
@@ -29,25 +29,23 @@ edit.startEditing(False, True)
 edit.startOperation()
 i = 0 #Variable para contar las iteraciones
 #Try
-try:
-    while True:
-        #Variables
-        MoveX = random.uniform(-15,15) #Genera numero aleatorio entre -"" y +"" y...
-        MoveY = random.uniform(-15,15) #...lo suma a las coordenadas del empleado.
-        #Actualizar coordenadas con el cursor
-        with arcpy.da.UpdateCursor(Employees, ["SHAPE@XY"]) as cursor:
-            for row in cursor:
-                cursor.updateRow([[row[0][0] + MoveX,row[0][1] + MoveY]])
-        #Suponemos que en la decima iteracion se reporta el accidente a los...
-        #empleados y dejan de moverse.
-        i += 1
-        if i == 6:
-            break
-    #Intervalo de tiempo en el que se "mueven" los empleados
-        time.sleep(10)
+while True:
+    #Variables
+    MoveX = random.uniform(-50,50) #Genera numero aleatorio entre -"50" y +"50"
+    MoveY = random.uniform(-50,50) #y se suma a las coordenadas del empleado.
+    #Actualizar coordenadas con el cursor
+    with arcpy.da.UpdateCursor(Employees, ["SHAPE@XY"]) as cursor:
+        for row in cursor:
+            cursor.updateRow([[row[0][0] + MoveX,row[0][1] + MoveY]])
+    #Suponemos que en la decima iteracion se reporta el accidente a los...
+    #empleados y dejan de moverse.
+    i += 1
+    if i == 1:
+        break
+#Intervalo de tiempo en el que se "mueven" los empleados
+
 #Except
-except arcpy.ExecuteError:
-    print "ERROR"
+
 #Stop the edit operation.
 edit.stopOperation()
 #Stop the edit session and save the changes
